17a17ca4985bff982d6643d1fe1f39e03cfac380
Tiago Meireles <tmmeireles@outlook.com>
2018-06-11 22:15:04 -0400

    install under snipeitapp user

--- snipeit.sh
+++ snipeit.sh
@@ -157,18 +157,18 @@ run_as () {
 install_composer () {
   # https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
   EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
-  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  run_as php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
   ACTUAL_SIGNATURE="$(php -r "echo hash_file('SHA384', 'composer-setup.php');")"
 
   if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
   then
       >&2 echo 'ERROR: Invalid composer installer signature'
-      rm composer-setup.php
+      run_as rm composer-setup.php
       exit 1
   fi
 
-  php composer-setup.php --install-dir "$webdir/$name/"
-  rm composer-setup.php
+  run_as php composer-setup.php
+  run_as rm composer-setup.php
 }
 
 install_snipeit () {
